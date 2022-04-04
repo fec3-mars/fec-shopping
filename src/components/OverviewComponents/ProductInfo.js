930b133d92a377625a99b17daa20fbdd3332b283
zcarpen <zcarpen@gmail.com>
2022-04-04 12:01:00 -0500

    starting product info

--- src/components/OverviewComponents/ProductInfo.js
+++ src/components/OverviewComponents/ProductInfo.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
-const ProductInfo = () => {
+const ProductInfo = ({ product, selectedStyle }) => {
 
+  console.log(product)
+  console.log(selectedStyle)
 
   return (
     <div className="container product-info">
-      <p>ProductInfo!</p>
+      <div className="product-info-reviews-container">
+        <p> stars component</p>
+        <span className="read-all-review">READ ALL 5 REVIEWS (needs to scroll to other comp)</span>
+      </div>
+      <h3>CATEGORY</h3>
+      <h1>{product.name}</h1>
     </div>
   )
 }
