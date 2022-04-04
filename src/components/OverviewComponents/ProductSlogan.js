ca93c0977f5ec80578c4bfb1e7a76f615ecc351f
zcarpen <zcarpen@gmail.com>
2022-04-04 17:13:41 -0500

    prepares to add features to ProductSlogan

--- src/components/OverviewComponents/ProductSlogan.js
+++ src/components/OverviewComponents/ProductSlogan.js
@@ -2,15 +2,23 @@ import React from 'react';
 
 const ProductSlogan = ({ product, selectedStyle }) => {
 
-  console.log(product)
-  console.log(selectedStyle)
+  // console.log(product)
+  // console.log(selectedStyle)
+  const features = product.features;
 
 
   return (
     <div data-testid="product-slogan" className="container product-slogan">
 
-      <h2>{product.slogan}</h2>
-      <p>{product.description}</p>
+      <div className="slogan-description">
+        <h2>{product.slogan}</h2>
+        <p>{product.description}</p>
+      </div>
+      <ul className="features">
+        {/* create a feature component */}
+
+
+      </ul>
 
     </div>
   )
