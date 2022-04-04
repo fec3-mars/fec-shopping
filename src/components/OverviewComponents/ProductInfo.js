3c340c6d897105c7d86e7c575e83428f1fcf28f0
zcarpen <zcarpen@gmail.com>
2022-04-04 12:53:45 -0500

    implements basic product info positioning (Price, Category, Reviews)

--- src/components/OverviewComponents/ProductInfo.js
+++ src/components/OverviewComponents/ProductInfo.js
@@ -1,18 +1,26 @@
 import React from 'react';
+import './ProductInfo.css';
 
 const ProductInfo = ({ product, selectedStyle }) => {
 
   console.log(product)
   console.log(selectedStyle)
+  const discount = selectedStyle.sale_price ? true : false;
+  let priceClass = discount ? "price slashed" : "price";
+
 
   return (
     <div className="container product-info">
       <div className="product-info-reviews-container">
-        <p> stars component</p>
-        <span className="read-all-review">READ ALL 5 REVIEWS (needs to scroll to other comp)</span>
+        <p> (***********)</p>
+        <span className="read-all-review">READ ALL 5 REVIEWS </span>
       </div>
-      <h3>CATEGORY</h3>
-      <h1>{product.name}</h1>
+      <p className="category">CATEGORY</p>
+      <h1 className="product-name">{product.name}</h1>
+      <div className="price-container">
+        <h4 className={priceClass}>${selectedStyle.original_price}</h4>
+        {discount && <h4 className="price-sale">${selectedStyle.sale_price}</h4>}
+      </div>
     </div>
   )
 }
