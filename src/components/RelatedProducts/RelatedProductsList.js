22cfe1b7ff145050367eec762fda19f1b1f77f69
Wendy Zhang <wzhang5252@gmail.com>
2022-04-05 14:52:45 -0400

    added css and comparison button

--- src/components/RelatedProducts/RelatedProductsList.js
+++ src/components/RelatedProducts/RelatedProductsList.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import App from '../App.js'
 import { axios, makeRequest, getRelatedProducts, getRelatedDetail, getRelatedImage } from '../axios.js';
+import './Related.css';
+import './star.jpeg';
 
 // this RelatedProductsList should stay the same for each currentproduct, thus shld be stored in global state
 //this list should gather all elements/component of related products to be rendered
@@ -10,44 +12,65 @@ class RelatedProducts extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      curProduct: props.curProduct,
       relatedProducts: [],
       relatedProductDetail: {},
       relatedProductImage: {}
     };
+    this.comparisonModal = this.comparisonModal.bind(this);
   }
 
-  componentDidMount() {
-    getRelatedProducts.call(this, 66642);
-    getRelatedDetail.call(this, [66643, 66644, 66649, 66648]);
-    getRelatedImage.call(this, [66643, 66644, 66649, 66648]);
+  // componentDidMount() {
+  //   getRelatedProducts.call(this, 66642);
+  //   getRelatedDetail.call(this, [66643, 66644, 66649, 66648]);
+  //   getRelatedImage.call(this, [66643, 66644, 66649, 66648]);
+  // }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.curProduct.data?.id !== prevProps.curProduct.data?.id) {
+      // console.log('curProduct at update', this.props.curProduct);
+      getRelatedProducts.call(this, this.props.curProduct.data.id);
+    }
+    if (this.state.relatedProducts !== prevState.relatedProducts) {
+      getRelatedDetail.call(this, this.state.relatedProducts);
+      getRelatedImage.call(this, this.state.relatedProducts);
+    }
   }
 
+  comparisonModal(){
+    console.log('clicked');
+    console.log('state in click', this.state);
+    // render(){
+    //   <div>
+    //     <h1>Comparion Modal</h1>
+    //   </div>
+    // }
+  }
 
   render() {
-    if (this.state.relatedProductDetail.length === this.state.relatedProducts.length) {
-      // console.log('state at related render', this.state.relatedProductImage);
+    if (this.state.relatedProductDetail.length === this.state.relatedProducts.length & this.state.relatedProductImage.length === this.state.relatedProducts.length) {
+
+      console.log('state to be rendered', this.state)
       return (
-        <div>
+        <div className='relatedProducts'>
           <h1>RELATED PRODUCTS</h1>
           <div className='relatedCard'>{this.state.relatedProductDetail.map((element, index) => {
             return (
-              <ul key={element.id} element={element}>
-                <h2 className='related category'>{element.category}</h2>
-                <img className='productImage' src={this.state.relatedProductImage[index]}></img>
-                <div className='related name'>{element.name}</div>
-                <div className='related price'>${element.default_price}</div>
-                <div className='related rating'>rating will go here</div>
-              </ul>
+              <div key={element.id} element={element} className='individualCard'>
+                <img className='relatedImage' src={this.state.relatedProductImage[index]}></img>
+                <button className='relatedButton' onClick={this.comparisonModal} ><img src='http://imgur.com/I0EwG.png'/></button>
+                <h2 className='relatedCategory'>{element.category}</h2>
+                <div className='relatedName'>{element.name}</div>
+                <div className='relatedPrice'>${element.default_price}</div>
+                <div className='relatedRating'>rating will go here</div>
+              </div>
             )
           })}
           </div>
         </div>
       )
+      // }
     }
   }
 }
-
 
 
 
