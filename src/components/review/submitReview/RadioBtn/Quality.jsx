37e78ba43afe47c565af170617acd8da8485eb16
Paul Koh <paul.s.koh@gmail.com>
2022-04-11 19:32:34 -0400

    updated radio buttons for submit review

--- src/components/review/submitReview/RadioBtn/Quality.jsx
+++ src/components/review/submitReview/RadioBtn/Quality.jsx
@@ -2,42 +2,42 @@
 import React from "react";
 import { useState } from "react";
 
-export default function RadioBtn_Quality({ setRating }) {
+export default function RadioBtn_Quality(props) {
   return (
     <div>
       <input
         type="radio"
         name="fit"
         value="1"
-        onClick={(e) => setRating(e.target.value)}
+        onClick={(e) => props.setChanged(e, "value")}
       />
       1 - Poor
       <input
         type="radio"
         name="fit"
         value="2"
-        onClick={(e) => setRating(e.target.value)}
+        onClick={(e) => props.setChanged(e, "value")}
       />
       2 - Below average
       <input
         type="radio"
         name="fit"
         value="3"
-        onClick={(e) => setRating(e.target.value)}
+        onClick={(e) => props.setChanged(e, "value")}
       />
       3 - What I expected
       <input
         type="radio"
         name="fit"
         value="4"
-        onClick={(e) => setRating(e.target.value)}
+        onClick={(e) => props.setChanged(e, "value")}
       />
       4 - Pretty great
       <input
         type="radio"
         name="fit"
         value="5"
-        onClick={(e) => setRating(e.target.value)}
+        onClick={(e) => props.setChanged(e, "value")}
       />
       5 - Perfect
     </div>
