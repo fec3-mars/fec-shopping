1584dcf62dc3cc93f3b643993abd29aaf2419f93
Lucas <lucasbonner@protonmail.com>
2022-04-12 12:30:50 -0500

    adds ds store to .gitignore, as well as adding img not found for photos

--- src/components/Q&A/IndividualPhoto/IndividualPhoto.jsx
+++ src/components/Q&A/IndividualPhoto/IndividualPhoto.jsx
@@ -21,16 +21,30 @@ class IndividualPhoto extends React.Component {
     this.setState({ show: false });
   };
 
+  onImageError( {currentTarget} ) {
+    currentTarget.onerror = null;
+    currentTarget.src = 'https://www.escapeauthority.com/wp-content/uploads/2116/11/No-image-found.jpg';
+  }
+
   render() {
     return (
       <div className="add-a-photo">
         <ImageModal show={this.state.show} handleClose={this.hideModal}>
-          <img src={this.props.photo} alt="" className="full-photo" />
+          <img
+            src={this.props.photo}
+            alt=""
+            className="full-photo"
+            onError={this.onImageError}
+          />
         </ImageModal>
 
         <li className="thumbnails">
           <button className="photo-button" type="button" onClick={this.showModal}>
-            <img src={this.props.photo} className="thumbnail" />
+            <img
+              src={this.props.photo}
+              className="thumbnail"
+              onError = {this.onImageError}
+            />
           </button>
         </li>
       </div>
